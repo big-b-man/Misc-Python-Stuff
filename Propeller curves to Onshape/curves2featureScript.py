cde9c95b33d88e2a3258cfe84d1afc59c023fc3c
bennett <diysandvich@gmail.com>
2025-03-14 12:45:14 -0700

    updated program to code complete script

--- Propeller curves to Onshape/curves2featureScript.py
+++ Propeller curves to Onshape/curves2featureScript.py
@@ -10,40 +10,65 @@ def LoadCSV(filePath):
         data = [[float(x) for x in row[:3]] for row in reader]
     return data
 
-code = str()
-code = code + 'FeatureScript 2599;\n' + 'import(path : "onshape/std/common.fs", version : "2599.0");\n\n'
-code = code + '//Name of feature so we can select it in the Part Studio\n' + 'annotation { "Feature Type Name" : "Prop Loft" }\n'
-code = code + 'export const propLoft = defineFeature(function(context is Context, id is Id, definition is map)\n'
-code = code + '    precondition{}\n' + '    {\n' + '        const offsets = [\n'
-
-for i in range (19): #number of curves minus 1
-    data = LoadCSV("SectionCurve" + str(i+1) + ".txt")
-    code = code + '                        ' + str(data[0][2]) + ' * meter,\n'
-
-#last code snipet doesn't have colon
-data = LoadCSV("SectionCurve" + str(20) + ".txt")
-code = code + '                        ' + str(data[0][2]) + ' * meter\n'
-code = code + '                        ];\n\n'
-code = code + '        const points = [[\n'
-
-for i in range (19): #number of curves minus 1
-    data = LoadCSV("SectionCurve" + str(i+1) + ".txt")
-    code = code + '                        //Curve ' + str(i+1) +'\n'
-    for j in range(len(data) - 1):
-        code = code + '                        vector('
-        code = code + str(data[j][0])+','+str(data[j][1]) + ') * meter,\n'
-    code = code + '                        vector('
-    code = code + str(data[len(data)-1][0])+','+str(data[len(data)-1][1]) + ') * meter\n'
-    code = code + '                    ],[\n'
-
-data = LoadCSV("SectionCurve" + str(20) + ".txt")
-code = code + '                        //Curve ' + str(20) +'\n'
-for j in range(len(data) - 1):
-    code = code + '                        vector('
-    code = code + str(data[j][0])+','+str(data[j][1]) + ') * meter,\n'
-code = code + '                        vector('
-code = code + str(data[len(data)-1][0])+','+str(data[len(data)-1][1]) + ') * meter\n'
-code = code + '                    ]];\n'
-
 with open("code.txt", 'w') as file:
-    file.write(code)
+    file.write('FeatureScript 2599;\n' + 'import(path : "onshape/std/common.fs", version : "2599.0");\n\n')
+
+with open("code.txt", 'a') as file:
+    file.write('//Name of feature so we can select it in the Part Studio\n')
+    file.write('annotation { "Feature Type Name" : "Prop Loft" }\n')
+    file.write('export const propLoft = defineFeature(function(context is Context, id is Id, definition is map)\n')
+    file.write('    precondition{}\n')
+    file.write('    {\n')
+    file.write('        const offsets = [\n')
+    code = str()
+
+    for i in range (19): #number of curves minus 1
+        data = LoadCSV("SectionCurve" + str(i+1) + ".txt")
+        file.write('                        ' + str(data[0][2]) + ' * meter,\n')
+
+    #last code snipet doesn't have colon
+    data = LoadCSV("SectionCurve" + str(20) + ".txt")
+    file.write('                        ' + str(data[0][2]) + ' * meter\n')
+    file.write('                        ];\n\n')
+    file.write('        const points = [[\n')
+
+    for i in range (19): #number of curves minus 1
+        data = LoadCSV("SectionCurve" + str(i+1) + ".txt")
+        file.write('                        //Curve ' + str(i+1) +'\n')
+        for j in range(len(data) - 1):
+            file.write('                        vector(')
+            file.write(str(data[j][0])+','+str(data[j][1]) + ') * meter,\n')
+        #last code snipet doesn't have colon
+        file.write('                        vector(')
+        file.write(str(data[len(data)-1][0])+','+str(data[len(data)-1][1]) + ') * meter\n')
+        file.write('                    ],[\n')
+    
+    #last code snipet doesn't have colon
+    data = LoadCSV("SectionCurve" + str(20) + ".txt")
+    file.write('                        //Curve ' + str(20) +'\n')
+    for j in range(len(data) - 1):
+        file.write('                        vector(')
+        file.write(str(data[j][0])+','+str(data[j][1]) + ') * meter,\n')
+    file.write('                        vector(')
+    file.write(str(data[len(data)-1][0])+','+str(data[len(data)-1][1]) + ') * meter\n')
+    file.write('                    ]];\n\n')
+    file.write('        //create sketch planes from imported data\n')
+    file.write('        var sketchId = id + "sketch";\n')
+    file.write('        for (var i = 0; i < 20; i += 1)\n')
+    file.write('        {\n')
+    file.write('            //define plane offset\n')
+    file.write('            var offsetDistance = offsets[i];\n')
+    file.write('            sketchId = sketchId + i;\n')
+    file.write('            // Define the offset plane (offset along Z-axis)\n')
+    file.write('            const offsetPlane = plane(vector(0*inch, 0*inch, offsetDistance), vector(0, 0, 1)*inch);\n')
+    file.write('            var sketch1 = newSketchOnPlane(context, sketchId, { "sketchPlane" : offsetPlane });\n')
+    file.write('            //sketch some stuff here\n')
+    file.write('            \n')
+    file.write('            skFitSpline(sketch1, "spline1", {\n')
+    file.write('                    "points" : points[i]\n')
+    file.write('            });\n')
+    file.write('        \n')
+    file.write('            // Finalize the sketch\n')
+    file.write('            skSolve(sketch1);\n')
+    file.write('        }\n')
+    file.write('    });\n')
