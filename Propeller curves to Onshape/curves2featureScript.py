d1a94ddcb3b5c83431f3d828e0db8870a7ac1ca1
bennett <diysandvich@gmail.com>
2025-03-24 02:08:16 -0700

    Put non-dynamic code into text files

--- Propeller curves to Onshape/curves2featureScript.py
+++ Propeller curves to Onshape/curves2featureScript.py
@@ -10,17 +10,13 @@ def LoadCSV(filePath):
         data = [[float(x) for x in row[:3]] for row in reader]
     return data
 
-with open("code.txt", 'w') as file:
-    file.write('FeatureScript 2599;\n' + 'import(path : "onshape/std/common.fs", version : "2599.0");\n\n')
+file = open("code.txt", 'w')
+file.close
+    
 
 with open("code.txt", 'a') as file:
-    file.write('//Name of feature so we can select it in the Part Studio\n')
-    file.write('annotation { "Feature Type Name" : "Prop Loft" }\n')
-    file.write('export const propLoft = defineFeature(function(context is Context, id is Id, definition is map)\n')
-    file.write('    precondition{}\n')
-    file.write('    {\n')
-    file.write('        const offsets = [\n')
-    code = str()
+    with open('preCode.txt', 'r') as preCode:
+        file.write(preCode.read())
 
     for i in range (19): #number of curves minus 1
         data = LoadCSV("SectionCurve" + str(i+1) + ".txt")
@@ -51,24 +47,5 @@ with open("code.txt", 'a') as file:
         file.write(str(data[j][0])+','+str(data[j][1]) + ') * meter,\n')
     file.write('                        vector(')
     file.write(str(data[len(data)-1][0])+','+str(data[len(data)-1][1]) + ') * meter\n')
-    file.write('                    ]];\n\n')
-    file.write('        //create sketch planes from imported data\n')
-    file.write('        var sketchId = id + "sketch";\n')
-    file.write('        for (var i = 0; i < 20; i += 1)\n')
-    file.write('        {\n')
-    file.write('            //define plane offset\n')
-    file.write('            var offsetDistance = offsets[i];\n')
-    file.write('            sketchId = sketchId + i;\n')
-    file.write('            // Define the offset plane (offset along Z-axis)\n')
-    file.write('            const offsetPlane = plane(vector(0*inch, 0*inch, offsetDistance), vector(0, 0, 1)*inch);\n')
-    file.write('            var sketch1 = newSketchOnPlane(context, sketchId, { "sketchPlane" : offsetPlane });\n')
-    file.write('            //sketch some stuff here\n')
-    file.write('            \n')
-    file.write('            skFitSpline(sketch1, "spline1", {\n')
-    file.write('                    "points" : points[i]\n')
-    file.write('            });\n')
-    file.write('        \n')
-    file.write('            // Finalize the sketch\n')
-    file.write('            skSolve(sketch1);\n')
-    file.write('        }\n')
-    file.write('    });\n')
+    with open('postCode.txt', 'r') as postCode:
+        file.write(postCode.read())
