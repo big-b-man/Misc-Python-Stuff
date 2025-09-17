b01b599f6ac03a88c4692ac8e5f86e7b0964fdee
bennett <diysandvich@gmail.com>
2025-09-17 00:30:38 -0700

    Continued progress on PDF fileds

--- Pool Data Sheet Filler/Data Sheet Filler.py
+++ Pool Data Sheet Filler/Data Sheet Filler.py
@@ -16,8 +16,12 @@
 
 import pymupdf
 import pandas as pd
+import os
 
 #General Style Notes: 12 Point font, offset 6 point from base of cell
+
+ExcelPath = "Pool-construction-permit-application-form.pdf"
+FilledPDFPath = "Filled Data Sheet.pdf"
 
 def safe_insert_text(page, position, value, **kwargs):
     # Handle NaN/NaT/missing values
@@ -29,20 +33,21 @@ def safe_insert_text(page, position, value, **kwargs):
     page.insert_text(position, value, **kwargs)
 
 def writedoc(doc, healthData):
-    #Page 2: Application to:
+    #Page 2
+    #Application to
     
     #Health Authority Checkbox
     match(healthData.iloc[1,1]):
         case "Fraser":
-            doc[1].draw_line([ 41.2, 149.2],[ 49.2, 157.9],color=(1,0,0),width=3)
+            doc[1].draw_line([ 41, 149],[ 49, 158],color=(1,0,0),width=3)
         case "Interior": 
-            doc[1].draw_line([161.7, 149.2],[169.7, 157.9],color=(1,0,0),width=3)
+            doc[1].draw_line([162, 149],[170, 158],color=(1,0,0),width=3)
         case "Coastal":
-            doc[1].draw_line([256.8, 149.2],[264.8, 157.9],color=(1,0,0),width=3)
+            doc[1].draw_line([257, 149],[265, 158],color=(1,0,0),width=3)
         case "Island":
-            doc[1].draw_line([369.3, 149.2],[377.3, 157.9],color=(1,0,0),width=3)
+            doc[1].draw_line([369, 149],[377, 158],color=(1,0,0),width=3)
         case "Northern":
-            doc[1].draw_line([481.9, 149.2],[489.9, 157.9],color=(1,0,0),width=3)
+            doc[1].draw_line([482, 149],[490, 158],color=(1,0,0),width=3)
         case _:
             print("Page 2: No Health Authority Defined")
 
@@ -63,12 +68,33 @@ def writedoc(doc, healthData):
     safe_insert_text(doc[1], [37,451], healthData.iloc[11,1], color=None, fontsize=12, overlay=True)
     safe_insert_text(doc[1], [254,451], healthData.iloc[11,3], color=None, fontsize=12, overlay=True)
 
+    #Page 3:
+    #General Information
+    safe_insert_text(doc[2], [103,146], healthData.iloc[14,1], color=None, fontsize=12, overlay=True)
+    safe_insert_text(doc[2], [103,172], healthData.iloc[15,1], color=None, fontsize=12, overlay=True)
+
+    #Pool Type Checkbox
+    match(healthData.iloc[16,1]):
+        case "Public Pool":
+            doc[2].draw_line([180, 186],[188, 195],color=(1,0,0),width=3)
+        case "Commercial Pool": 
+            doc[2].draw_line([282, 186],[290, 195],color=(1,0,0),width=3)
+        case "Hot Tub":
+            doc[2].draw_line([343, 186],[351, 195],color=(1,0,0),width=3)
+        case "Spray Pool":
+            doc[2].draw_line([414, 186],[422, 195],color=(1,0,0),width=3)
+        case "Wading Pool":
+            doc[2].draw_line([497, 186],[505, 195],color=(1,0,0),width=3)
+        case _:
+            print("Page 3: No Pool Type Defined")
+
     for page in doc:
         page.insert_text([0,12], "Hello World!", fontsize=12, color=None, overlay=True)
 
-doc = pymupdf.open("Pool-construction-permit-application-form.pdf")
+doc = pymupdf.open(ExcelPath)
 excelData = pd.read_excel("Test Sheet.xlsx",sheet_name="Pool Data For Export", header=None)
 print ("Excel Data read:\n",excelData)
 writedoc(doc,excelData)
-doc.save("Filled Data Sheet.pdf")
+doc.save(FilledPDFPath)
 doc.close()
+os.startfile(FilledPDFPath)
