fa151f3f6c4ae3a09ca6a1b4f2c26b9fb2b4fa09
bennett <diysandvich@gmail.com>
2025-09-18 23:39:58 -0700

    A lot of error checking

--- Pool Data Sheet Filler/Data Sheet Filler.py
+++ Pool Data Sheet Filler/Data Sheet Filler.py
@@ -18,13 +18,149 @@
 import pymupdf
 import pandas as pd
 import os
-from dataclasses import dataclass
+import sys
+import json
+from tkinter import messagebox
+import jsonschema
 
 #General Style Notes: 12 Point font, offset 6 point from base of cell
 
 ExcelPath = "Pool-construction-permit-application-form.pdf"
 FilledPDFPath = "Filled Data Sheet.pdf"
 
+#Load JSON, Check if
+def parseConfigFile(filepath):
+    #load configuration file, throw error if not found
+    try:
+        with open(filepath, 'r') as FILE:
+            data = json.load(FILE)
+    except Exception as e:
+        messagebox.showwarning("Error", "Error code 1: Error reading config: {str(e)}")
+        sys.exit()
+    
+    return data
+
+#Checks if the configuration file contains "default settings and fields"
+def checkConfigSchema(data):
+    schema = {
+        "type": "object",
+        "required": ["default settings", "fields"]
+    }
+
+    try:
+        jsonschema.validate(instance=data, schema=schema)
+        print("Configuration file contains default data and fields objects")
+    except jsonschema.ValidationError as e:
+        messagebox.showwarning("Error", "Error code 2: " + str(e.message) + "\n")
+        sys.exit()
+    checkDefaultSettings(data["default settings"])
+    checkFields(data["fields"])
+
+#checks if the default settings are formatte correctly
+def checkDefaultSettings(data):
+    schema = {
+        "type": "object",
+        "required": ["font size",
+                     "text x offset",
+                     "text y offset",
+                     "checkbox size"],
+        "properties": {
+        "font size": {"type": "number"},
+        "text x offset": {"type": "number"},
+        "text y offset": {"type": "number"},
+        "checkbox size": {"type": "number"}
+        }
+    }
+
+    try:
+        jsonschema.validate(instance=data, schema=schema)
+        print("default settings are valid")
+    except jsonschema.ValidationError as e:
+        messagebox.showwarning("Error", "Error code 3: " + str(e.message) + "\n")
+        sys.exit()
+
+#checks if inputed data fields are formatted correctly
+def checkFields(data):
+    i = 1
+    for item in data:
+        match item["type"]:
+            case "string":
+                validateString(item,i)
+                i += 1
+            case "multiChoiceCheckbox":
+                validateMultiChoiceCheckbox(item,i)
+                i += 1
+            case _:
+                messagebox.showwarning("Error", "Error code 4: document field "+ str(i) + " is an invalid entry type")
+                sys.exit()
+
+#checks if string fields are formatted correctly
+def validateString(data, num):
+    schema = {
+        "type": "object",
+        "required": ["name",
+            "type",
+            "cell reference",
+            "document page",
+            "document coordinates",
+            "coordinate units"],
+        "properties": {
+        "name": {"type": "string"},
+        "cell reference": {"type": "string"},
+        "document page": {"type": "number"},
+        "document coordinates": {
+            "type": "array",
+            "items": {"type": "number"},
+            "minItems": 2,
+            "maxItems": 2
+        },
+        "coordinate units": {
+            "type": "string",
+            "enum": ["points", "inches", "mm"]
+            }
+        }
+    }
+    try:
+        jsonschema.validate(instance=data, schema=schema)
+    except jsonschema.ValidationError as e:
+        messagebox.showwarning("Error", "Error code 5: document field "+ str(num) +" has invalid schema: " + str(e.message) + "\n")
+        sys.exit()
+
+def validateMultiChoiceCheckbox(data, num):
+    schema = {
+        "type": "object",
+        "required": ["name",
+            "type",
+            "options",
+            "document coordinates",
+            "cell reference",
+            "document page",
+            "coordinate units"],
+        "properties": {
+        "name": {"type": "string"},
+        "options": {"type": "array"},
+        "document coordinates": {"type": "array"},
+        "document page": {"type": "number"},
+        "coordinate units": {
+            "type": "string",
+            "enum": ["points", "inches", "mm"]
+            }
+        }
+    }
+    try:
+        jsonschema.validate(instance=data, schema=schema)
+    except jsonschema.ValidationError as e:
+        messagebox.showwarning("Error", "Error code 6: document field "+ str(num) +" has invalid schema: " + str(e.message) + "\n")
+        sys.exit()
+    if(len(data["document coordinates"])==len(data["options"])):
+        print("yes")
+    else:
+        #
+        #
+        #PICK UP HERE AND WRITE ERROR CODE AND VERIFY ALL STRINGS IN OPTIONS AND ALL NUMBERS IN COORDINATES
+        #
+        #
+        print("no")
 
 def safe_insert_text(page, position, value, **kwargs):
     # Handle NaN/NaT/missing values
@@ -36,64 +172,10 @@ def safe_insert_text(page, position, value, **kwargs):
     page.insert_text(position, value, **kwargs)
 
 def writedoc(doc, healthData):
-    #Page 2
-    #Application to
-    
-    #Health Authority Checkbox
-    match(healthData.iloc[1,1]):
-        case "Fraser":
-            doc[1].draw_line([ 41, 149],[ 49, 158],color=(1,0,0),width=3)
-        case "Interior": 
-            doc[1].draw_line([162, 149],[170, 158],color=(1,0,0),width=3)
-        case "Coastal":
-            doc[1].draw_line([257, 149],[265, 158],color=(1,0,0),width=3)
-        case "Island":
-            doc[1].draw_line([369, 149],[377, 158],color=(1,0,0),width=3)
-        case "Northern":
-            doc[1].draw_line([482, 149],[490, 158],color=(1,0,0),width=3)
-        case _:
-            print("Page 2: No Health Authority Defined")
+    print("test")
 
-    #Pool Name, Date, and Address
-    safe_insert_text(doc[1], [37,210], healthData.iloc[2,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [412,210], healthData.iloc[2,3], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [37,239], healthData.iloc[3,1], color=None, fontsize=12, overlay=True)
-
-    #Owner Info
-    safe_insert_text(doc[1], [37,289], healthData.iloc[5,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [37,318], healthData.iloc[6,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [37,345], healthData.iloc[7,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [254,345], healthData.iloc[7,3], color=None, fontsize=12, overlay=True)    
-
-    #Person Applying for Permit Info
-    safe_insert_text(doc[1], [37,395], healthData.iloc[9,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [37,422], healthData.iloc[10,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [37,451], healthData.iloc[11,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[1], [254,451], healthData.iloc[11,3], color=None, fontsize=12, overlay=True)
-
-    #Page 3:
-    #General Information
-    safe_insert_text(doc[2], [103,146], healthData.iloc[14,1], color=None, fontsize=12, overlay=True)
-    safe_insert_text(doc[2], [103,172], healthData.iloc[15,1], color=None, fontsize=12, overlay=True)
-
-    #Pool Type Checkbox
-    match(healthData.iloc[16,1]):
-        case "Public Pool":
-            doc[2].draw_line([180, 186],[188, 195],color=(1,0,0),width=3)
-        case "Commercial Pool": 
-            doc[2].draw_line([282, 186],[290, 195],color=(1,0,0),width=3)
-        case "Hot Tub":
-            doc[2].draw_line([343, 186],[351, 195],color=(1,0,0),width=3)
-        case "Spray Pool":
-            doc[2].draw_line([414, 186],[422, 195],color=(1,0,0),width=3)
-        case "Wading Pool":
-            doc[2].draw_line([497, 186],[505, 195],color=(1,0,0),width=3)
-        case _:
-            print("Page 3: No Pool Type Defined")
-
-    for page in doc:
-        page.insert_text([0,12], "Hello World!", fontsize=12, color=None, overlay=True)
-
+configuration = parseConfigFile("Configuration.json")
+checkConfigSchema(configuration)
 doc = pymupdf.open(ExcelPath)
 excelData = pd.read_excel("Test Sheet.xlsx",sheet_name="Pool Data For Export", header=None)
 print ("Excel Data read:\n",excelData)
